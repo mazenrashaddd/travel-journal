3469fda6af75074bfc95331083bf6ecb68f3283c
Mazen Rashad <mazenrashadedu@gmail.com>
2025-01-16 12:45:03 +0200

    GitHub deployment on netlify

--- src/Components/Entry/entryData.js
+++ src/Components/Entry/entryData.js
@@ -5,7 +5,7 @@ const entryData = [
       src: "https://scrimba.com/links/travel-journal-japan-image-url",
       alt: "Mount Fuji",
     },
-    title: "Mount Saint",
+    title: "Mount Fuji",
     country: "Japan",
     googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
     dates: "12 Jan, 2021 - 24 Jan, 2021",
